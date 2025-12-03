2dfb2c0b01ae71c9cdd6b358f8dbd7c32a8e9f7d
Hasan Abuzayed <hasanabuzayed@gmail.com>
2025-12-02 18:38:51 -0600

    Rename plugin to Mersal Breakdance Elements

--- plugin.php
+++ plugin.php
@@ -1,25 +1,25 @@
 <?php
 
 /**
- * Plugin Name: Breakdance Custom Elements
- * Plugin URI: https://breakdance.com/
- * Description: Boilerplate plugin to save your custom elements created with Element Studio.
- * Author: Breakdance
- * Author URI: https://breakdance.com/
+ * Plugin Name: Mersal Breakdance Elements
+ * Plugin URI: https://mersalparfums.com/
+ * Description: Mersal Elements built using Breakdance Element Studio
+ * Author: Mersal Parfums, LLC
+ * Author URI: https://mersalparfums.com/
  * License: GPLv2
- * Text Domain: breakdance
+ * Text Domain: mersalparfums
  * Domain Path: /languages/
  * Version: 0.0.1
  */
 
-namespace BreakdanceCustomElements;
+namespace MersalBreakdanceElements;
 
 use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
 
 add_action('breakdance_loaded', function () {
     \Breakdance\ElementStudio\registerSaveLocation(
         getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
-        'BreakdanceCustomElements',
+        'MersalBreakdanceElements',
         'element',
         'Custom Elements',
         false
@@ -27,7 +27,7 @@ add_action('breakdance_loaded', function () {
 
     \Breakdance\ElementStudio\registerSaveLocation(
         getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
-        'BreakdanceCustomElements',
+        'MersalBreakdanceElements',
         'macro',
         'Custom Macros',
         false,
@@ -35,7 +35,7 @@ add_action('breakdance_loaded', function () {
 
     \Breakdance\ElementStudio\registerSaveLocation(
         getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
-        'BreakdanceCustomElements',
+        'MersalBreakdanceElements',
         'preset',
         'Custom Presets',
         false,
